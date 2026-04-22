38fb5ef4536d7db1c09cae013ee124f23cad1591
Illyasviel <zy99306@outlook.com>
2026-04-22 16:37:59 +0800

    update shell path setup for Homebrew and rbenv

--- app_configs/paths.sh
+++ app_configs/paths.sh
@@ -1,22 +1,22 @@
 export XDG_CONFIG_HOME="$HOME/.config"
 
-if [ -d /usr/local/lib ]; then
-  APPEND_LD_LIBRARY_PATH="/usr/local/lib:/usr/local/lib64"
-  if [[ $LD_LIBRARY_PATH != *"$APPEND_LD_LIBRARY_PATH"* ]]; then
-    if [ ! $LD_LIBRARY_PATH ]; then
-      export LD_LIBRARY_PATH=$APPEND_LD_LIBRARY_PATH
-    else
-      export LD_LIBRARY_PATH=$APPEND_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
-    fi
-  fi
-fi
+# if [ -d /usr/local/lib ]; then
+#   APPEND_LD_LIBRARY_PATH="/usr/local/lib:/usr/local/lib64"
+#   if [[ $LD_LIBRARY_PATH != *"$APPEND_LD_LIBRARY_PATH"* ]]; then
+#     if [ ! $LD_LIBRARY_PATH ]; then
+#       export LD_LIBRARY_PATH=$APPEND_LD_LIBRARY_PATH
+#     else
+#       export LD_LIBRARY_PATH=$APPEND_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+#     fi
+#   fi
+# fi
 
-if [[ $(uname -s) == "Linux" ]]; then
-  SYS_PATH="$HOME/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/bin"
-  if [[ $PATH != *"$SYS_PATH"* ]]; then
-    export PATH="$SYS_PATH:${PATH}"
-  fi
-fi
+# if [[ $(uname -s) == "Linux" ]]; then
+#   SYS_PATH="$HOME/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/bin"
+#   if [[ $PATH != *"$SYS_PATH"* ]]; then
+#     export PATH="$SYS_PATH:${PATH}"
+#   fi
+# fi
 
 append_path "$HOME/.local/bin"
 
@@ -27,5 +27,11 @@ if [[ $(uname -s) == "Darwin" ]]; then
 fi
 
 if [[ $(uname -s) == "Darwin" ]]; then
-  prepend_path "/usr/local/opt/coreutils/libexec/gnubin"
+  eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+  prepend_path "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
+fi
+
+if [[ $(command_exists rbenv) == true ]]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init - zsh)"
 fi
