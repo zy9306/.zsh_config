ae2c78f4e8f8bc553235d5428edcf043f7e2a6a2
Illyasviel <zy99306@outlook.com>
2026-04-23 16:22:46 +0800

    update shell path setup for Flutter tooling

--- app_configs/paths.sh
+++ app_configs/paths.sh
@@ -22,6 +22,8 @@ append_path "$HOME/.local/bin"
 
 append_path "/usr/sbin"
 
+append_path "/opt/flutter/bin"
+
 if [[ $(uname -s) == "Darwin" ]]; then
 	export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/lib
 fi
