858b896024ed753dc5d1d536bd4660cce581a4bd
Illyasviel <zy99306@outlook.com>
2026-07-23 20:15:57 +0800

    update bat theme

--- app_configs/bat.sh
+++ app_configs/bat.sh
@@ -1,4 +1,5 @@
 if command_exists bat; then
-	export BAT_THEME="1337" # use GitHub for white theme, zenburn for black theme.
+	# export BAT_THEME="1337" # use GitHub for white theme, zenburn for black theme.
+	export BAT_THEME="GitHub"
 	alias cat="bat -p --wrap character"
 fi
